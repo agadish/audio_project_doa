8104381642e4c93cbe38200e86fd2713ccf53737
Assaf Gadish <agadish@gmail.com>
2024-05-10 12:45:31 +0300

    Fixed torch.cat dim error

--- unet_dac.py
+++ unet_dac.py
@@ -19,33 +19,34 @@ class UnetDAC(nn.Module):
         # In the encoder, convolutional layers with the Conv2d function are used to extract features from the input image. 
         # Each block in the encoder consists of two convolutional layers followed by a max-pooling layer, with the exception of the last block which does not include a max-pooling layer.
         # -------
-        # input: 572x572x3
-        self.e11 = nn.Conv2d(2 * (M - 1), 16, kernel_size=3, padding=1) # output: 570x570x64
-        self.e12 = nn.Conv2d(16, 16, kernel_size=3, padding=1) #paoutput: 568x568x64
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 284x284x64
+        # input: 64x14x256x96 (Batch_size=64 x MicsFactor=14 x FreqBins=256 x Frames~96)
+        self.e11 = nn.Conv2d(2 * (M - 1), 16, kernel_size=3, padding=1) # output: 64x16x256x96
+        self.e12 = nn.Conv2d(16, 16, kernel_size=3, padding=1) # output: 64x16x256x96
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 64x16x128x48
 
         # input: 284x284x64
-        self.e21 = nn.Conv2d(16, 32, kernel_size=3, padding=1) # output: 282x282x128
-        self.e22 = nn.Conv2d(32, 32, kernel_size=3, padding=1) # output: 280x280x128
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 140x140x128
+        self.e21 = nn.Conv2d(16, 32, kernel_size=3, padding=1) # output: 64x32x128x48
+        self.e22 = nn.Conv2d(32, 32, kernel_size=3, padding=1) # output: 64x32x128x48
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 64x32x64x24
 
         # input: 140x140x128
-        self.e31 = nn.Conv2d(32, 64, kernel_size=3, padding=1) # output: 138x138x256
-        self.e32 = nn.Conv2d(64, 64, kernel_size=3, padding=1) # output: 136x136x256
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 68x68x256
+        self.e31 = nn.Conv2d(32, 64, kernel_size=3, padding=1) # output: 64x64x64x24
+        self.e32 = nn.Conv2d(64, 64, kernel_size=3, padding=1) # output: 64x64x64x24
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 64x64x32x12
 
         # input: 68x68x256
-        self.e41 = nn.Conv2d(64, 128, kernel_size=3, padding=1) # output: 66x66x512
-        self.e42 = nn.Conv2d(128, 128, kernel_size=3, padding=1) # output: 64x64x512
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 32x32x512
+        self.e41 = nn.Conv2d(64, 128, kernel_size=3, padding=1) # output: 64x128x32x12
+        self.e42 = nn.Conv2d(128, 128, kernel_size=3, padding=1) # output: 64x128x32x12
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 64x128x16x6
 
         # input: 32x32x512
-        self.e51 = nn.Conv2d(128, 256, kernel_size=3, padding=1) # output: 30x30x1024
-        self.e52 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 28x28x1024
+        self.e51 = nn.Conv2d(128, 256, kernel_size=3, padding=1) # output: 64x256x16x6
+        self.e52 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 64x256x16x6
 
 
         # Decoder
-        self.upconv1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.upconv1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2) # output: 64x128x32x12
+        # Cat with 64x128x32x12 --> 64x256x32x12
         self.d11 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
         self.d12 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
 
@@ -86,8 +87,8 @@ class UnetDAC(nn.Module):
         xe52 = F.elu(self.dropout(self.e52(xe51)))
         
         # Decoder
-        xu1 = self.upconv1(xe52)
-        xu11 = torch.cat([xu1, xe42], dim=0) # XXX: Changed from dim=1 in all places.
+        xu1 = self.upconv1(xe52) # 
+        xu11 = torch.cat([xu1, xe42], dim=1) # XXX: Changed from dim=1 in all places.
         """
         xu1.shape
         torch.Size([128, 32, 2])
@@ -102,24 +103,24 @@ class UnetDAC(nn.Module):
         xd12 = F.elu(self.dropout(self.d12(xd11)))
 
         xu2 = self.upconv2(xd12)
-        xu22 = torch.cat([xu2, xe32], dim=0)
+        xu22 = torch.cat([xu2, xe32], dim=1)
         xd21 = F.elu(self.dropout(self.d21(xu22)))
         xd22 = F.elu(self.dropout(self.d22(xd21)))
 
         xu3 = self.upconv3(xd22)
-        xu33 = torch.cat([xu3, xe22], dim=0)
+        xu33 = torch.cat([xu3, xe22], dim=1)
         xd31 = F.elu(self.dropout(self.d31(xu33)))
         xd32 = F.elu(self.dropout(self.d32(xd31)))
 
         xu4 = self.upconv4(xd32)
-        xu44 = torch.cat([xu4, xe12], dim=0)
+        xu44 = torch.cat([xu4, xe12], dim=1)
         xd41 = F.elu(self.dropout(self.d41(xu44)))
         xd42 = F.elu(self.dropout(self.d42(xd41)))
 
         # Output layer
         out = self.outconv(xd42)
 
-        softmax_res = F.softmax(out, dim=0) # dim=0 refers to the 13 microphones
+        softmax_res = F.softmax(out, dim=1) # dim=1 refers to the 13 microphones
 
         return softmax_res
     
